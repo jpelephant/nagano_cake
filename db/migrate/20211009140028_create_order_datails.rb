0af72ce5cf72ef755c31a03a6be46b3d86177bb8
EC2 Default User <ec2-user@ip-172-31-44-114.ec2.internal>
2021-10-10 13:56:37 +0000

    [Add]migrateファイル

---
+++ db/migrate/20211009140028_create_order_datails.rb
@@ -0,0 +1,13 @@
+class CreateOrderDatails < ActiveRecord::Migration[5.2]
+  def change
+    create_table :order_datails do |t|
+      t.integer :order_id,      null: false
+      t.integer :item_id,       null: false
+      t.integer :price,         null: false 
+      t.integer :amount,        null: false
+      t.integer :making_status, null: false
+
+      t.timestamps        null: false
+    end
+  end
+end
