d179aca57fd48c6d469463083ae80135676fa334
EC2 Default User <ec2-user@ip-172-31-44-114.ec2.internal>
2021-10-13 06:07:22 +0000

    [Add]ファイル

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,31 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :costomers
+  
+  get 'homes/about' => "homes#about"
+  root to: 'homes#top'
+  resources :items, only: [:index, :show]
+  resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :customers, except: [:new, :index, :update]
+  get 'customers' => 'customers#withdrawal'
+  resources :cart_items, only: [:update, :create, :destroy]
+  delete 'cart_items' => 'cart_items#destroy_all'
+  resources :orders, only: [:new, :index, :show]
+  get 'orders' => 'orders#verification'
+  get 'orders' => 'orders#thanks'
+  patch 'orders' => 'orders#confirm'
+  resources 'shipping_oaddress'
+  
+  namespace admin do
+    resources :sessions, only: [:new, :create, :destroy]
+    get 'homes' => 'homes#top'
+    resources :items, except: [:destroy]
+    resources :genres, except: [:new, :show, :destroy]
+    resources :customers, except: [:new, :create, :destroy]
+    get 'order_show' => 'order_show#show'
+    patch 'orders' => 'orders#update'
+    patch 'order_datails' => 'order_datails#up'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
